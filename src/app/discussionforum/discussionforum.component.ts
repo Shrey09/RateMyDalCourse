714691575ce2f6b9e6204ebc92694d437719e6a3
hpamnani <harshpam1993@gmail.com>
2019-06-19 03:08:57 -0300

    navbar working on editprofile, discussion, and dashboard

--- src/app/discussionforum/discussionforum.component.ts
+++ src/app/discussionforum/discussionforum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-discussionforum',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discussionforum.component.css']
 })
 export class DiscussionforumComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
+    this.authenticationService.authenticate();
   }
+
+  constructor(
+    public authenticationService: AuthenticationService,
+  ) {
+  }
+
 
 }
--- src/app/editprofile/editprofile.component.ts
+++ src/app/editprofile/editprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
+    this.authenticationService.authenticate();
   }
+
+  constructor(
+    public authenticationService: AuthenticationService,
+  ) {
+  }
+
 
 }
