57c0ea89bb8e6b565ecb873608af703224132780
Chintan Patel <chintan97patel@gmail.com>
2019-06-19 11:52:50 -0300

    Validation added in editprofile

---
+++ src/app/editprofile/confirm-equal-validator.directive.ts
@@ -0,0 +1,24 @@
+import { Validator, NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+    selector:'[appConfirmEqualValidator]',
+    providers:[{
+        provide:NG_VALIDATORS,
+        useExisting:ConfirmValidatorDirective,
+        multi:true
+    }]
+})
+export class ConfirmValidatorDirective implements Validator{
+    @Input() appConfirmEqualValidator:string
+    validate(control:AbstractControl):{[key:string]:any}|null{
+        const controlToCompare=control.parent.get(this.appConfirmEqualValidator);
+        if (controlToCompare && controlToCompare.value!==control.value){
+            return{'notEqual':true};
+        }
+        else{
+            return null;
+        }
+        
+    }
+}
---
+++ src/app/editprofile/user.ts
@@ -0,0 +1,7 @@
+export class User{
+    constructor(
+        public name:string,
+        public password:string,
+        public confirmPassword:string,
+    ){}
+}
