60a5c0862b1ddafa6cd4876d88b1e76673e6fd41
hpamnani <harshpam1993@gmail.com>
2019-07-12 20:34:37 -0300

    server address changed to bluenose

--- Group3_Tech_Intellect/server/server.js
+++ Group3_Tech_Intellect/server/server.js
@@ -4,7 +4,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
-const port=3000;
+const port=24125;
 
 // Initializing express and telling the app to user bodyParser
 const app=express();
